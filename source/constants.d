c4ff6a91e3b19c4f5be2985b8c6ffcecf331aff6
Antonio-M. Corbi Bellot <antonio.corbi@ua.es>
2017-06-06 10:57:31 +0200

    We are working with ui files and not with resurces.
    These files had to be changed/deleted.

--- source/constants.d
+++
@@ -1,5 +0,0 @@
-
-module constants;
-
-public immutable UIDIR = "/home/acorbi/Documentos/programas/dlang/gtkdapp/data/ui/";
-public immutable DATADIR = "/home/acorbi/Documentos/programas/dlang/gtkdapp/data/";
